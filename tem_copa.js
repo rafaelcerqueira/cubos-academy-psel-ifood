b523fbc6f846d18752d890d0d00e770e5b7a4b0d
Rafael Freitas <rafaelcerqueira92@gmail.com>
2021-06-08 19:09:46 -0300

    Delete tem_copa.js

--- tem_copa.js
+++
@@ -1,9 +0,0 @@
-function solucao(ano) {
-    if(ano > 2021 && ano % 2 == 1){
-        console.log("MEH")
-    } else if (ano > 2021 && ano % 4 == 0){
-        console.log('JOGOS')
-    } else {
-        console.log('COPA')
-    }
-}
