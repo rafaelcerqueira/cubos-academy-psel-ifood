ab114246a0f6cb18acbc6fdb2ffc271e60966a67
Rafael Freitas <rafaelcerqueira92@gmail.com>
2021-06-08 18:44:59 -0300

    desafio 2

---
+++ me_segue_no_insta.js
@@ -0,0 +1,10 @@
+function solucao(largura, altura) {
+      if (largura < altura){
+          console.log('RETRATO');
+      } else if (largura > altura){
+          console.log('PAISAGEM');
+      } else {
+          console.log('QUADRADA');
+      };
+    
+  };
